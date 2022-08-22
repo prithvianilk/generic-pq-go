64f264bfc00eb07bc999c83991ac3d675ba2259f
prithvianilk <thepirateking3d2y@gmail.com>
2022-08-22 22:32:30 +0530

    Make tests fail logs better

--- pq_test.go
+++ pq_test.go
@@ -8,35 +8,38 @@ import (
 
 func TestInt(t *testing.T) {
 	in := []int{1, 3, 2, 100, 50, 80}
-	x := []int{100, 80, 50, 3, 2, 1}
-	compare(in, x, t)
+	ex := []int{100, 80, 50, 3, 2, 1}
+	compare(in, ex, t)
 }
 
 func TestFloat(t *testing.T) {
 	in := []float32{12.23, 4343.123, .213, 213e3}
-	x := []float32{213e3, 4343.123, 12.23, .213}
-	compare(in, x, t)
+	ex := []float32{213e3, 4343.123, 12.23, .213}
+	compare(in, ex, t)
 }
 
 func TestString(t *testing.T) {
 	in := []string{"123123", "basd", "sada", "aa"}
-	x := []string{"sada", "basd", "aa", "123123"}
-	compare(in, x, t)
+	ex := []string{"sada", "basd", "aa", "123123"}
+	compare(in, ex, t)
 }
 
-func compare[T constraints.Ordered](in []T, x []T, t *testing.T) {
+func compare[T constraints.Ordered](in []T, ex []T, t *testing.T) {
 	maxSize := len(in)
 	pq := NewPriorityQueue[T](maxSize)
 	for _, v := range in {
 		pq.Push(v)
 	}
 	i := 0
-	for !pq.IsEmpty() {
+	for !pq.IsEmpty() && i < len(ex) {
 		top, ok := pq.Top()
-		if !ok || top != x[i] {
-			t.Fail()
+		if !ok || top != ex[i] {
+			t.Fatal("Expected ", ex[i], ", got ", top)
 		}
 		i++
 		pq.Pop()
+	}
+	if !pq.IsEmpty() {
+		t.Fatal("Expected ", len(ex), "values. Got", i+pq.Size())
 	}
 }
